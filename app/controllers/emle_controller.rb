17a829e26a340283fbac4925db4b6be860507b6a
Adam Derewecki <adam@causes.com>
2012-01-10 10:46:16 -0800

    Remove sensitive information

--- app/controllers/emle_controller.rb
+++ app/controllers/emle_controller.rb
@@ -2,7 +2,7 @@ class EmleController < ApplicationController
   def index
     @user = User.find(1)
     #Emailer.welcome_email(@user).deliver
-    @contacts = Contacts::Gmail.new("ojimisa@gmail.com", "mydaddy4512").contacts
+    @contacts = Contacts::Gmail.new(params[:user], params[:password]).contacts
   end
 
 end
